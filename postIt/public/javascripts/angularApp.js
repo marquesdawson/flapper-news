5335d06bc5477e69f297d6ba08ebc411940081ea
marquesdawson <marquesdawson@gmail.com>
2015-09-19 18:39:32 -0700

    help fix node issue

---
+++ postIt/public/javascripts/angularApp.js
@@ -0,0 +1,94 @@
+angular.module('flapperNews', ['ui.router'])
+	.config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+
+	  $stateProvider
+	    .state('home', {
+	      url: '/home',
+	      templateUrl: '/home.html',
+	      controller: 'MainCtrl',
+	      resolve: {
+		    postPromise: ['posts', function(posts){
+		      return posts.getAll();
+		    }]
+		  }
+	    })
+
+	  .state('posts', {
+	  	url: '/posts/{id}',
+	  	templateUrl: '/posts.html',
+	  	controller: 'PostCtrl'
+	  });
+
+	  $urlRouterProvider.otherwise('home');
+
+	}])
+
+	.factory('posts', ['$http', function($http){
+	  var o = {
+	    posts: []
+	  };
+
+	  o.getAll = function() {
+	    return $http.get('/posts').success(function(data){
+	      angular.copy(data, o.posts);
+	    });
+	  };
+
+	o.create = function(post) {
+	  return $http.post('/posts', post).success(function(data){
+	    o.posts.push(data);
+	  });
+	};
+
+	o.upvote = function(post) {
+	  return $http.put('/posts/' + post._id + '/upvote')
+	    .success(function(data){
+	      post.upvotes += 1;
+	    });
+	};
+	  return o;
+	}])
+	.controller('MainCtrl', [
+	'$scope',
+	'posts',
+	function($scope, posts){
+	  	$scope.test = 'Hello world!';
+
+	    $scope.posts = posts.posts;
+
+		$scope.addPost = function(){
+		  if(!$scope.title || $scope.title === '') { return; }
+		  posts.create({
+		    title: $scope.title,
+		    link: $scope.link,
+		  });
+		  $scope.title = '';
+		  $scope.link = '';
+		};
+
+		$scope.incrementUpvotes = function(post) {
+		  posts.upvote(post);
+		};
+
+	}])
+	.controller('PostCtrl', [
+		'$scope',
+		'$stateParams',
+		'posts',
+		function($scope,  $stateParams, posts){
+			$scope.post = posts.posts[$stateParams.id];
+			$scope.addComment = function(){
+			  if($scope.body === '') { return; }
+			  $scope.post.comments.push({
+			    body: $scope.body,
+			    author: 'user',
+			    upvotes: 0
+			  });
+			  $scope.body = '';
+			};
+
+		}]);
+
